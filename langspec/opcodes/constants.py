61d7074d25da171c16e007f8699ab10c5902b3ea
Rayan Hatout <rayan.hatout@gmail.com>
2022-01-23 02:15:44 +0000

    Implement fuzzing

---
+++ langspec/opcodes/constants.py
@@ -0,0 +1,92 @@
+import random
+from typing import TYPE_CHECKING, Generator, List, Union
+from uuid import uuid4
+
+from langspec.opcodes import (
+    Constant,
+    OpCode,
+)
+
+if TYPE_CHECKING:
+    from langspec.opcodes.context import Context
+from langspec.opcodes.types.abstract_types import (
+    NumericalType,
+    Type,
+)
+from langspec.opcodes.types.concrete_types import (
+    OpTypeBool,
+)
+from patched_dataclass import dataclass
+
+
+class OpConstantTrue(Constant):
+    type: Type = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.type = OpTypeBool()
+        return [self.type, self]
+
+
+class OpConstantFalse(Constant):
+    type: Type = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.type = OpTypeBool()
+        return [self.type, self]
+
+
+@dataclass
+class OpConstant(Constant):
+    type: Type = None
+    value: Union[int, float] = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+
+        self.type = NumericalType().fuzz(context)[0]
+        from langspec.opcodes.types.concrete_types import OpTypeInt
+
+        if isinstance(self.type, OpTypeInt):
+            if self.type.signed:
+                self.value = random.randint(
+                    -(2 ** (self.type.width - 1)), 2 ** (self.type.width - 1) - 1
+                )
+            else:
+                self.value = random.randint(0, 2 ** self.type.width)
+        else:
+            self.value = random.uniform(0, 2 ** self.type.width)
+
+        return [self.type, self]
+
+
+# @dataclass
+# class OpConstantComposite(Constant):
+#     constants: Tuple[Constant]
+
+#     def fuzz(self,  tvc, context: Context) -> List[OpCode]:
+#
+#         self.type: Union[MixedContainerType, UniformContainerType] = random.choice(
+#             list(
+#                 filter(
+#                     lambda tvc: isinstance(tvc, ContainerType)
+#                     and not isinstance(tvc, OpTypePointer),
+#                     TVC_table.keys(),
+#                 )
+#             )
+#         )
+#         self.constants = []
+#         if isinstance(self.type, UniformContainerType):
+#             for _ in range(len(self.type)):
+#                 constant: Constant = find_constant(tvc, self.type.type,  context)
+#                 self.constants.append(constant)
+#         else:
+#             for type in self.type.types:
+#                 constant: Constant = find_constant(tvc, type,  context)
+#                 self.constants.append(constant)
+#         self.constants = tuple(self.constants)
+#         return [self]
+
+#     def to_spasm(self, TVC_table) -> str:
+#         spasm = f"%{self.id} = OpConstantComposite %{TVC_table[self.type]}"
+#         for constant in self.constants:
+#             spasm += f" %{TVC_table[constant]}"
+#         return spasm
---
+++ langspec/opcodes/types/concrete_types.py
@@ -0,0 +1,140 @@
+import random
+import sys
+from typing import TYPE_CHECKING, Generator, List, Tuple
+from uuid import uuid4
+
+from langspec.enums import StorageClass
+from langspec.opcodes import (
+    FuzzLeaf,
+    OpCode,
+    OpCode,
+)
+
+if TYPE_CHECKING:
+    from langspec.opcodes.context import Context, TVC
+from langspec.opcodes.types.abstract_types import (
+    ArithmeticType,
+    ContainerType,
+    MixedContainerType,
+    NumericalType,
+    ScalarType,
+    Type,
+    UniformContainerType,
+)
+from patched_dataclass import dataclass
+
+
+class OpTypeVoid(FuzzLeaf, Type):
+    pass
+
+
+class OpTypeInt(ScalarType, NumericalType, ArithmeticType):
+    width: int = None
+    signed: int = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.signed = int(bool(random.getrandbits(1)))
+        self.width = 2 ** 5  # TODO other widths with capabilities
+        return [self]
+
+
+class OpTypeFloat(ScalarType, NumericalType, ArithmeticType):
+    width: int = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.width = 2 ** 5  # TODO other widths with capabilities
+        return [self]
+
+
+class OpTypeBool(FuzzLeaf, ScalarType):
+    pass
+
+
+class OpTypeVector(UniformContainerType, ArithmeticType):
+    type: Type = None
+    size: int = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.size = random.choice([2, 3, 4])  # 8, 16])
+        self.type = ScalarType().fuzz(context)[0]
+        return [self.type, self]
+
+    def __len__(self):
+        return self.size
+
+
+class OpTypePointer(UniformContainerType):
+    storage_class: StorageClass = None
+    type: Type = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.storage_class = random.choice(list(StorageClass))
+        fuzzed_type = random.choice([ScalarType, ContainerType])().fuzz(context)
+        self.type = fuzzed_type[-1]
+        if self.type is None:
+            print("??????")
+        return [*fuzzed_type, self]
+
+
+class OpTypeFunction(Type):
+    return_type: Type = None
+    parameter_types: Tuple[Type] = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+
+        # return_type = random.choice([ScalarType, ContainerType])().fuzz(context)
+        return_type = [OpTypeVoid()]
+        self.return_type = return_type[-1]
+        self.parameter_types = []
+        all_types = return_type
+        for _ in range(random.randint(0, 5)):
+            parameter_type = random.choice([ScalarType, ContainerType])().fuzz(context)
+            self.parameter_types.append(parameter_type[-1])
+            all_types += parameter_type
+        self.parameter_types = tuple(self.parameter_types)
+        return [*all_types, self]
+
+
+class OpTypeArray(UniformContainerType):
+    type: Type = None
+    length: OpCode = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+
+        self.type = ScalarType().fuzz(context)[0]
+        from langspec.opcodes.constants import OpConstant
+
+        self.length = OpConstant()
+        self.length.type = OpTypeInt()
+        self.length.type.signed = 0
+        self.length.type.width = 32
+        self.length.value = random.randint(1, 32)
+        return [self.type, self.length.type, self.length, self]
+
+    def __len__(self):
+        return self.length.value
+
+
+class OpTypeStruct(MixedContainerType):
+    types: Tuple[Type] = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.types = []
+        side_effect_types = []
+        for _ in range(random.randint(0, 5)):
+            parameter_type = random.choice([ScalarType, ContainerType])().fuzz(context)
+            self.types.append(parameter_type[-1])
+            side_effect_types += parameter_type
+        self.types = tuple(self.types)
+        return [*side_effect_types, *self.types, self]
+
+    def __len__(self):
+        return len(self.types)
+
+
+class OpTypeRuntimeArray(UniformContainerType):
+    type: Type = None
+
+    def fuzz(self, context: "Context") -> List[OpCode]:
+        self.type = ScalarType().fuzz(context)[0]
+        return [self.type, self]
