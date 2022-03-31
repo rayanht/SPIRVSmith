461baaf45b1f5e84e14ec0bd5e94d791979b3443
Rayan Hatout <rayan.hatout@gmail.com>
2022-03-31 08:08:59 +0100

    Add logic operations and parametrize randomness

--- run.py
+++ run.py
@@ -7,15 +7,25 @@ from fuzzer import ShaderGenerator
 
 @dataclass
 class SPIRVSmithConfig:
+    # Binaries
     ASSEMBLER_PATH: str = "bin/spirv-as"
     VALIDATOR_PATH: str = "bin/spirv-val"
     OPTIMISER_PATH: str = "bin/spirv-opt"
     AMBER_PATH: str = "bin/amber"
     
-    n_types: int = 10
-    n_constants: int = 4
+    # Limits
+    n_types: int = 15
+    n_constants: int = 10
     n_functions: int = 1
     max_depth: int = 3
+    
+    # Randomness parametrization (weights)
+    w_memory_operation: int = 5
+    w_logical_operation: int = 6
+    w_arithmetic_operation: int = 6
+    w_control_flow_operation: int = 1
+    w_function_operation: int = 1
+    p_statement: float = 0.98
 
 cs = ConfigStore.instance()
 
