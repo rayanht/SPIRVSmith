25e5069741c508e43430a638dad43a7637e7d141
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-08 14:13:32 +0100

    Add BiqQuery instance and PubSub to infra

--- infra/main.tf
+++ infra/main.tf
@@ -73,3 +73,79 @@ EOT
     network = google_compute_network.default_network.name
   }
 }
+
+resource "google_bigquery_dataset" "spirv_dataset" {
+  dataset_id                  = "spirv"
+  friendly_name               = "Shaders metadata"
+  location                    = "US"
+  default_table_expiration_ms = 3600000
+}
+
+resource "google_bigquery_table" "spirv_metadata_table" {
+  dataset_id          = google_bigquery_dataset.spirv_dataset.dataset_id
+  table_id            = "metadata"
+  deletion_protection = false # Schema is subject to change for now
+
+  schema = <<EOF
+[
+  {
+    "name": "shader_id",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "Shader identifier. Not a primary key."
+  },
+  {
+    "name": "expected_reports",
+    "type": "INT64",
+    "mode": "REQUIRED",
+    "description": "Number of reports to be expected by the aggregator node before checking for a mismatch."
+  },
+  {
+    "name": "buffer_dump",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "A single reported buffer dump. Initially NULL."
+  },
+  {
+    "name": "platform_os",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "One of Linux or MacOS"
+  },
+  {
+    "name": "platform_hardware_type",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "One of Intel CPU/AMD CPU/Nvidia GPU/Radeon GPU"
+  },
+  {
+    "name": "platform_hardware_info",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "Detailed hardware report (i.e. driver version, hardware model etc...)"
+  },
+  {
+    "name": "platform_backend",
+    "type": "STRING",
+    "mode": "NULLABLE",
+    "description": "e.g. Vulkan or SwiftShader"
+  },
+]
+EOF
+
+}
+resource "google_pubsub_schema" "spirv_shader_pubsub_schema" {
+  name       = "spirv_shader_pubsub_schema"
+  type       = "PROTOCOL_BUFFER"
+  definition = "syntax = \"proto3\";\nmessage ShaderGenerated {\nstring shader_id = 1;\n}"
+}
+
+resource "google_pubsub_topic" "spirv_shader_pubsub_topic" {
+  name = "spirv_shader_pubsub_topic"
+
+  depends_on = [google_pubsub_schema.spirv_shader_pubsub_schema]
+  schema_settings {
+    schema   = "projects/spirvsmith/schemas/spirv_shader_pubsub_schema"
+    encoding = "JSON"
+  }
+}
