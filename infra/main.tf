bb23fd129647fe741acb2d7802a1836164171965
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-08 16:46:17 +0100

    Create PubSub subscription

--- infra/main.tf
+++ infra/main.tf
@@ -149,3 +149,24 @@ resource "google_pubsub_topic" "spirv_shader_pubsub_topic" {
     encoding = "JSON"
   }
 }
+
+resource "google_pubsub_subscription" "example" {
+  name  = "spirv_shader_pubsub_subscription"
+  topic = google_pubsub_topic.spirv_shader_pubsub_topic.name
+
+  # 20 minutes
+  message_retention_duration = "1200s"
+  retain_acked_messages      = true
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+
+  retry_policy {
+    minimum_backoff = "10s"
+  }
+
+  enable_message_ordering = false
+}
