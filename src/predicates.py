d1131ba81c4dc7f08fdf40240994feee7b7b6b77
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-16 04:45:23 +0100

    feat(fuzzer): implement fuzzing of OpCompositeExtract, OpCompositeInsert, OpCopyObject, and OpTranspose

--- src/predicates.py
+++ src/predicates.py
@@ -1,17 +1,22 @@
 from src.enums import StorageClass
 from src.types.abstract_types import ArithmeticType
 from src.types.concrete_types import (
+    OpTypeArray,
     OpTypeBool,
     OpTypeFloat,
     OpTypeInt,
     OpTypeMatrix,
+    OpTypeStruct,
     OpTypeVector,
 )
 
 IsVectorType = lambda x: isinstance(x.type, OpTypeVector)
+IsArrayType = lambda x: isinstance(x.type, OpTypeArray)
 IsMatrixType = lambda x: isinstance(x.type, OpTypeMatrix)
+IsStructType = lambda x: isinstance(x.type, OpTypeStruct)
 IsScalarInteger = lambda x: isinstance(x.type, OpTypeInt)
 IsScalarFloat = lambda x: isinstance(x.type, OpTypeFloat)
+IsCompositeType = lambda x: isinstance(x.type, (OpTypeMatrix, OpTypeVector, OpTypeStruct, OpTypeArray))
 HasValidBaseType = lambda x, target_type: isinstance(x.get_base_type(), target_type)
 HasValidSign = (
     lambda x, signed: x.get_base_type().signed == signed if signed is not None else True
