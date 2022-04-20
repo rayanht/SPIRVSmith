2339b5592ad16c7bbd0859617685cec2ee2b6dd1
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-20 03:28:22 +0100

    fix(infra): inject Google Cloud credentials when running the Docker image

--- scripts/run_cloud.sh
+++ scripts/run_cloud.sh
@@ -1,8 +1,6 @@
 #!bash
 DD_AGENT_MAJOR_VERSION=7 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 mv datadog_conf.yaml /etc/datadog-agent/conf.d/python.d/conf.yaml
-git clone https://github.com/Gallore/yaml_cli
-cd yaml_cli
-pip install .
-yaml_cli -f /etc/datadog-agent/datadog.yaml -b logs_enabled true
+echo $GOOGLE_CREDENTIALS | base64 -d -o infra/spirvsmith_gcp.json
+yq -yi .logs_enabled=true /etc/datadog-agent/datadog.yaml
 python3 run.py hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=none hydra/hydra_logging=none
