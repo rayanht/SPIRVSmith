b9732e80baf1d062d0f3a665f2f29e9503b27ed0
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-20 04:00:59 +0100

    fix(cloud-build): use GNU version of base64 in cloud starttup script

--- scripts/run_cloud.sh
+++ scripts/run_cloud.sh
@@ -1,6 +1,7 @@
 #!bash
 DD_AGENT_MAJOR_VERSION=7 DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 mv datadog_conf.yaml /etc/datadog-agent/conf.d/python.d/conf.yaml
-echo $GOOGLE_CREDENTIALS | base64 -d -o infra/spirvsmith_gcp.json
 yq -yi .logs_enabled=true /etc/datadog-agent/datadog.yaml
+touch infra/spirvsmith_gcp.json
+echo $GOOGLE_CREDENTIALS | base64 -d > infra/spirvsmith_gcp.json
 python3 run.py hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=none hydra/hydra_logging=none
