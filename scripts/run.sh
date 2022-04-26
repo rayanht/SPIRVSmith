aae9f36b773f1ce1ba35c33ad9bc46c078e131bc
Rayan Hatout <rayan.hatout@gmail.com>
2022-04-26 21:31:09 +0100

    feat(infra): introduce DataDog tracing and make BigQuery tables permanent

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!bash
-python3 run.py hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=none hydra/hydra_logging=none
+DD_SERVICE="spirvsmith" DD_ENV="prod" DD_LOGS_INJECTION=true DD_PROFILING_ENABLED=true ddtrace-run python3 run.py hydra.run.dir=. hydra.output_subdir=null hydra/job_logging=none hydra/hydra_logging=none
