75dfb52f370285b96eba98bdc1efc031f7708a8d
Rayan Hatout <rayan.hatout@gmail.com>
2022-05-17 06:01:40 +0100

    feat(reducer): add an interestingness test for segfaults

--- interestingness_segfault.py
+++ interestingness_segfault.py
@@ -1,20 +1,25 @@
 import argparse
+import tempfile
 import time
 from tempfile import NamedTemporaryFile
 
 import pandas as pd
 
+from amber_client import run_amber
 from src.shader_brokerage import BQ_delete_shader
 from src.shader_brokerage import BQ_fetch_reduced_buffer_dumps
 from src.shader_brokerage import BQ_insert_new_shader
 from src.shader_brokerage import GCS_upload_shader
 from src.shader_parser import parse_spirv_assembly_file
+from src.shader_utils import create_amber_file
 from src.shader_utils import disassemble_spv_file
 from src.shader_utils import SPIRVShader
 from src.shader_utils import validate_spv_file
 
 
-def is_interesting(spv_file_path: str, n_reports: int) -> int:
+def is_interesting(
+    spv_file_path: str, shader_id: str, n_buffers: int, seed: int
+) -> int:
     if not validate_spv_file(spv_file_path):
         return 1
     with NamedTemporaryFile(suffix=".spasm") as disassembled_spasm_file:
@@ -24,30 +29,27 @@ def is_interesting(spv_file_path: str, n_reports: int) -> int:
             disassembled_spasm_file.name
         )
         parsed_shader = parsed_shader.recondition().normalise_ids()
-        if not parsed_shader.validate():
-            return 1
-        GCS_upload_shader(parsed_shader)
-        BQ_insert_new_shader(parsed_shader, "reducer", high_priority=True)
-        print(f"Inserted temporary shader with id {parsed_shader.id}")
-        while True:
-            buffer_dumps: pd.Series = (
-                BQ_fetch_reduced_buffer_dumps(parsed_shader.id)
-                .to_dataframe()
-                .buffer_dump
+        with tempfile.NamedTemporaryFile(suffix=".amber") as amber_file:
+            create_amber_file(parsed_shader, amber_file.name, seed=seed)
+            buffer_dump: str = run_amber(
+                amber_filename=amber_file.name,
+                n_buffers=n_buffers,
+                shader_id=shader_id,
             )
-            if len(buffer_dumps) >= n_reports:
-                break
-            time.sleep(1)
-        BQ_delete_shader(parsed_shader.id)
-        if not all(x == buffer_dumps[0] for x in buffer_dumps):
-            return 0
+            if buffer_dump == "SEGFAULT":
+                return 0
         return 1
 
 
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("shader_id", help="Shader DB indentifier")
-    argparser.add_argument("n_reports", help="Number of executor reports to wait for")
+    argparser.add_argument("n_buffers", help="Number of buffers")
+    argparser.add_argument("seed", help="Randomness seed")
     argparser.add_argument("input_file", help="Path to a .spv file")
     args = argparser.parse_args()
-    exit(is_interesting(args.input_file, int(args.n_reports)))
+    exit(
+        is_interesting(
+            args.input_file, args.shader_id, int(args.n_buffers), int(args.seed)
+        )
+    )
---
+++ scripts/interestingness_segfault.sh
@@ -0,0 +1,1 @@
+python3 interestingness_segfault.py $1 $2 $3 $4
